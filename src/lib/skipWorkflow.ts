cbc84f456ca950881340ab1d2a511cad7a1182fc
Saul Maldonado <samaldonado4@gmail.com>
2020-10-30 18:06:53 -0500

    fix: docs workflow

--- src/lib/skipWorkflow.ts
+++ src/lib/skipWorkflow.ts
@@ -1,30 +1,29 @@
-// import { getInput } from '@actions/core';
-// import { getOctokit, context } from '@actions/github';
-// import { OctokitResponse } from '@octokit/types/dist-types/OctokitResponse';
-// import { actionConfig } from '../config';
+import { getInput } from '@actions/core';
+import { getOctokit, context } from '@actions/github';
+import { OctokitResponse } from '@octokit/types/dist-types/OctokitResponse';
+import { actionConfig } from '../config';
 
 import { getWorkflowRunId } from './actionContext';
 
-// const { GITHUB_TOKEN_ID } = actionConfig;
+const { GITHUB_TOKEN_ID } = actionConfig;
 
-// type SkipWorkflow = () => Promise<OctokitResponse<any> | never>;
+type SkipWorkflow = () => Promise<OctokitResponse<any> | never>;
 
-export const skipWorkflow = () => {
+export const skipWorkflow: SkipWorkflow = () => {
   try {
-    // const githubToken: string = getInput(GITHUB_TOKEN_ID);
+    const githubToken: string = getInput(GITHUB_TOKEN_ID);
 
     const runId: number = getWorkflowRunId();
 
     console.log(runId);
 
-    // const { actions } = getOctokit(githubToken);
+    const { actions } = getOctokit(githubToken);
 
-    // const {
-    //   repo: { owner, repo },
-    // } = context;
+    const {
+      repo: { owner, repo },
+    } = context;
 
-    throw new Error();
-    // return actions.cancelWorkflowRun({ run_id: runId, owner, repo });
+    return actions.cancelWorkflowRun({ run_id: runId, owner, repo });
   } catch (error) {
     throw new Error('❌ Error skipping workflow');
   }
