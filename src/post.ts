7ef034753ea0b732e782b8ef2c32ef058c3d0bce
Saul Maldonado <samaldonado4@gmail.com>
2020-10-30 15:57:02 -0500

    fix: docs workflow

--- src/post.ts
+++ src/post.ts
@@ -1,14 +1,14 @@
-import { green } from 'chalk';
-import { checkHeadCommit } from './lib/checkHeadCommit';
+// import { green } from 'chalk';
+// import { checkHeadCommit } from './lib/checkHeadCommit';
 
 type Post = () => Promise<void>;
 
 const post: Post = async () => {
   console.log('☑ Creating check...');
 
-  const sha = await checkHeadCommit();
+  // const sha = await checkHeadCommit();
 
-  console.log(green(`✅ Head commit sha: ${sha} checked as "success"`));
+  // console.log(green(`✅ Head commit sha: ${sha} checked as "success"`));
 };
 
 post();
