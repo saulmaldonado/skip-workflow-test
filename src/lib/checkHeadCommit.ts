fe5ad87e28aa2c243e8f62afb81c0630e2171b49
Saul Maldonado <samaldonado4@gmail.com>
2020-11-02 14:25:48 -0600

    fix: docs workflow

--- src/lib/checkHeadCommit.ts
+++ src/lib/checkHeadCommit.ts
@@ -9,7 +9,7 @@ const { GITHUB_TOKEN_ID } = actionConfig;
 
 export const checkHeadCommit: CheckHeadCommit = async () => {
   const githubToken = getInput(GITHUB_TOKEN_ID);
-  const { request } = getOctokit(githubToken);
+  const { checks } = getOctokit(githubToken);
 
   const {
     repo: { owner, repo },
@@ -21,43 +21,34 @@ export const checkHeadCommit: CheckHeadCommit = async () => {
     head: { sha },
   } = pull_request!;
 
-  const result = await request(
-    'PUT /repos/{owner}/{repo}/actions/workflows/{workflow_id}/disable',
-    {
-      owner,
-      repo,
-      workflow_id: workflow,
-    }
-  );
+  const result1 = await checks.listForRef({
+    owner,
+    repo,
+    ref: sha,
+    status: 'in_progress',
+  });
 
-  // const result1 = await checks.listForRef({
-  //   owner,
-  //   repo,
-  //   ref: sha,
-  //   status: 'in_progress',
-  // });
+  const checkId = result1.data.check_runs[0].id;
 
-  // const checkId = result1.data.check_runs[0].id;
-
-  // const result = await checks.update({
-  //   check_run_id: checkId,
-  //   owner,
-  //   repo,
-  //   status: 'completed',
-  //   conclusion: 'success',
-  // });
+  const result = await checks.update({
+    check_run_id: checkId,
+    owner,
+    repo,
+    status: 'completed',
+    conclusion: 'success',
+  });
 
   console.log(result);
 
-  // await checks.create({
-  //   head_sha: sha,
-  //   name: workflow,
-  //   owner,
-  //   repo,
-  //   /* must be ISO 8601 format https://docs.github.com/en/free-pro-team@latest/rest/reference/checks#create-a-check-run */
-  //   completed_at: new Date().toISOString(),
-  //   conclusion: 'success',
-  // });
+  await checks.create({
+    head_sha: sha,
+    name: workflow,
+    owner,
+    repo,
+    /* must be ISO 8601 format https://docs.github.com/en/free-pro-team@latest/rest/reference/checks#create-a-check-run */
+    completed_at: new Date().toISOString(),
+    conclusion: 'success',
+  });
 
   return sha;
 };
