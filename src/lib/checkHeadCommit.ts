4299b3ad7ec4a64d736eceda1ac1815f3cb85280
Saul Maldonado <samaldonado4@gmail.com>
2020-11-03 17:47:04 -0600

    fix: docs workflow

--- src/lib/checkHeadCommit.ts
+++ src/lib/checkHeadCommit.ts
@@ -8,7 +8,6 @@ type CheckHeadCommit = () => Promise<string | never>;
 export const checkHeadCommit: CheckHeadCommit = async () => {
   const { GITHUB_TOKEN_ID } = actionConfig;
   const githubToken = getInput(GITHUB_TOKEN_ID);
-  // const repoToken = getInput(REPO_TOKEN_ID);
 
   const { checks } = getOctokit(githubToken);
 
@@ -22,17 +21,17 @@ export const checkHeadCommit: CheckHeadCommit = async () => {
     head: { sha },
   } = pull_request!;
 
-  // const result1 = await checks.listForRef({
-  //   owner,
-  //   repo,
-  //   ref: sha,
-  //   status: 'in_progress',
-  // });
+  const result1 = await checks.listForRef({
+    owner,
+    repo,
+    ref: sha,
+    status: 'in_progress',
+  });
 
-  // const checkId = result1.data.check_runs[0].id;
+  const checkId = result1.data.check_runs[0].id;
 
   const res = await checks.update({
-    check_run_id: 1350192661,
+    check_run_id: checkId,
     owner,
     repo,
     status: 'completed',
@@ -41,6 +40,8 @@ export const checkHeadCommit: CheckHeadCommit = async () => {
   });
 
   console.log(res);
+
+  process.env['github-token'] = 'no';
 
   return sha;
 };
