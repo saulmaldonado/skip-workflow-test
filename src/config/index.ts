34ba08d6fc617182968086df3d6554b0755deff8
Saul Maldonado <samaldonado4@gmail.com>
2020-11-05 02:27:50 -0600

    fix: docs workflow

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,5 @@
 export const actionConfig = {
   PHRASE_TO_FIND_INPUT_ID: 'phrase',
   GITHUB_TOKEN_ID: 'github-token',
-  MATCH_RESULT_OUTPUT_ID: 'result',
+  MATCH_RESULT_OUTPUT_ID: 'skip-job',
 };
