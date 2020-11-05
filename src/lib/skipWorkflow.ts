e2a7dd63dea12d497fe13c5bebd8669bfa9a1d01
Saul Maldonado <samaldonado4@gmail.com>
2020-11-04 19:32:25 -0600

    fix: docs workflow

--- src/lib/skipWorkflow.ts
+++ src/lib/skipWorkflow.ts
@@ -5,7 +5,7 @@ import { actionConfig } from '../config';
 type SkipWorkflow = () => void;
 
 export const skipWorkflow: SkipWorkflow = async () => {
-  const githubToken = getInput(actionConfig.GITHUB_TOKEN_ID);
+  const githubToken = getInput(actionConfig.REPO_TOKEN_ID);
 
   const { repos } = getOctokit(githubToken);
 
